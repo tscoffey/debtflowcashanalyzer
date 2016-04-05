715f83e39b7cc1d730f7e37661126e7ed97761a4
Tim <Tim@tim-coffeys-mac-mini.local>
2016-04-04 22:07:03 -0400

    Initial Commit

---
+++ DebtCashFlowAnalyzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DebtCashFlowAnalyzer
+//
+//  Created by Tim on 4/4/16.
+//  Copyright © 2016 Coffey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
